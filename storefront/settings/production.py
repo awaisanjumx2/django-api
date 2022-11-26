59b7fdd5564fcb59630c9630b495b63768e441da
Awais Anjum <awaisanjum@MacBook-Pro.local>
2022-11-26 16:49:28 +0500

    Added DB configuration

--- storefront/settings/production.py
+++ storefront/settings/production.py
@@ -1,6 +1,17 @@
+import os
 from .common import *
 
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['mybuy-prod.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS']
+    }
+}
